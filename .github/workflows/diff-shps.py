9c8801fbb335680920bacd29cba738241a3f966e
Michal Migurski <mike@teczno.com>
2021-02-21 11:28:23 -0800

    General cleanup

--- .github/workflows/diff-shps.py
+++ .github/workflows/diff-shps.py
@@ -1,70 +1,71 @@
 #!/usr/bin/env python3
 import os
-import pprint
 import itertools
+import tempfile
+import logging
 import requests
 import dotenv
+import osgeo.ogr
 
 dotenv.load_dotenv()
+logging.basicConfig(level=logging.INFO)
 
 GIS_EXTENSIONS = ('.cpg', '.dbf', '.prj', '.shp', '.shx')
 
-if __name__ == '__main__':
+splitext_base = lambda path: os.path.splitext(path)[0]
+splitext_ext = lambda path: os.path.splitext(path)[1].lower()
+
+def list_compare_stems(env):
     compare_url = '{api_url}/repos/{repo}/compare/{base_ref}...{head_ref}'.format(
-        api_url=os.environ['GITHUB_API_URL'],
-        repo=os.environ['GITHUB_REPOSITORY'],
-        base_ref=os.environ['GITHUB_BASE_REF'],
-        head_ref=os.environ['GITHUB_HEAD_REF'],
+        api_url=env['GITHUB_API_URL'],
+        repo=env['GITHUB_REPOSITORY'],
+        base_ref=env['GITHUB_BASE_REF'],
+        head_ref=env['GITHUB_HEAD_REF'],
     )
     
     got_compare = requests.get(compare_url, headers={
-        'Authorization': 'token {token}'.format(token=os.environ['API_ACCESS_TOKEN']),
+        'Authorization': 'token {token}'.format(token=env['API_ACCESS_TOKEN']),
     })
     
     paths = [
         f['filename']
         for f in got_compare.json().get('files', [])
-        if os.path.splitext(f['filename'])[1].lower() in GIS_EXTENSIONS
+        if splitext_ext(f['filename']) in GIS_EXTENSIONS
     ]
     
-    stem_key = lambda path: os.path.splitext(path)[0]
+    stems = {splitext_base(path) for path in paths}
     
-    for (path_stem, group) in itertools.groupby(sorted(paths), stem_key):
-        dirname = os.path.dirname(path_stem)
-        print(path_stem, dirname)
-        
-        dir_base_url = '{api_url}/repos/{repo}/contents/{dir}?ref={base_ref}'.format(
-            api_url=os.environ['GITHUB_API_URL'],
-            repo=os.environ['GITHUB_REPOSITORY'],
-            base_ref=os.environ['GITHUB_BASE_REF'],
-            dir=dirname,
-        )
-        
-        got_dir_base = requests.get(dir_base_url, headers={
-            'Authorization': 'token {token}'.format(token=os.environ['API_ACCESS_TOKEN']),
-        })
-        
-        pprint.pprint({
-            f['path']: f['download_url']
-            for f in got_dir_base.json()
-            if os.path.splitext(f['path'])[0] == path_stem
-            and os.path.splitext(f['path'])[1].lower() in GIS_EXTENSIONS
-        })
-        
-        dir_head_url = '{api_url}/repos/{repo}/contents/{dir}?ref={head_ref}'.format(
-            api_url=os.environ['GITHUB_API_URL'],
-            repo=os.environ['GITHUB_REPOSITORY'],
-            head_ref=os.environ['GITHUB_HEAD_REF'],
-            dir=dirname,
-        )
-        
-        got_dir_head = requests.get(dir_head_url, headers={
-            'Authorization': 'token {token}'.format(token=os.environ['API_ACCESS_TOKEN']),
-        })
-        
-        pprint.pprint({
-            f['path']: f['download_url']
-            for f in got_dir_head.json()
-            if os.path.splitext(f['path'])[0] == path_stem
-            and os.path.splitext(f['path'])[1].lower() in GIS_EXTENSIONS
-        })
+    return sorted(list(stems))
+
+def list_ref_paths(env, path_stem, ref):
+    logging.info(f'{path_stem} @{ref}')
+
+    dir_url = '{api_url}/repos/{repo}/contents/{dir}?ref={ref}'.format(
+        api_url=env['GITHUB_API_URL'],
+        repo=env['GITHUB_REPOSITORY'],
+        dir=os.path.dirname(path_stem),
+        ref=ref,
+    )
+    
+    got_dir = requests.get(dir_url, headers={
+        'Authorization': 'token {token}'.format(token=env['API_ACCESS_TOKEN']),
+    })
+    
+    files = {
+        f['path']: f['download_url']
+        for f in got_dir.json()
+        if splitext_base(f['path']) == path_stem
+        and splitext_ext(f['path']) in GIS_EXTENSIONS
+    }
+    
+    for url in sorted(files.values()):
+        logging.info(f'{url}')
+    
+    return files
+
+if __name__ == '__main__':
+    
+    for stem in list_compare_stems(os.environ):
+        base_files = list_ref_paths(os.environ, stem, os.environ['GITHUB_BASE_REF'])
+        head_files = list_ref_paths(os.environ, stem, os.environ['GITHUB_HEAD_REF'])
+
